b190fb831d5e66a6b53bec54218c7e8edaaf8b8d
Source <abc@def.com>
2024-02-12 19:18:17 +0530

    PDF Vewer changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PdfViewerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PdfViewerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
