d0432bbdbcb1d3705523d33e2181bdb32d2fc937
Source <abc@def.com>
2024-07-12 15:53:19 +0530

    Initial Commit For PDF viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'adobe-pdf-viewer';
+  title = 'pdf-viewer';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AdobePdfViewerComponent } from './adobe-pdf-viewer/adobe-pdf-viewer.component';
+import { Ng2PdfViewerComponent } from './ng2-pdf-viewer/ng2-pdf-viewer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: AdobePdfViewerComponent,
+      title: 'Adobe-dc-viewer',
+    },
+    {
+        path: 'adobe-pdf-viewer',
+        component: AdobePdfViewerComponent,
+        title: 'Adobe-dc-viewer',
+      },
+    {
+      path: 'ng2viewer',
+      component: Ng2PdfViewerComponent,
+      title: 'Ng2-pdf-viewer',
+    },
+  ];
